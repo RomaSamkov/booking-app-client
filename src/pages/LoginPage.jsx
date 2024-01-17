b069ea74582bd0add75519f30cd084ddd3553d45
RomaSamkov <Romansamkov.rs@gmail.com>
2024-01-17 20:28:56 +0200

    add login form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,16 @@
 export default function LoginPage() {
   return (
-    <div>
-      <form action=""></form>
+    <div className="mt-4 flex items-center justify-around grow">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <form className="max-w-md mx-auto">
+          <input type="email" name="" id="" placeholder="Your Email@com" />
+          <input type="password" name="" id="" placeholder="Enter Password" />
+          <button type="submit" className="primary">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
