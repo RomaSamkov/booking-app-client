dfa62e765ff7a0cf6b31dc3a297ab2d47dab5182
RomaSamkov <Romansamkov.rs@gmail.com>
2024-01-24 17:19:42 +0200

    add onLoginUser

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,38 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  async function onLoginUser(e) {
+    e.preventDefault();
+    try {
+      await axios.post("/login", { email, password });
+      alert("Login is succesfull");
+    } catch (error) {
+      alert("Login is failed");
+    }
+  }
   return (
     <div className="mt-4 flex items-center justify-around grow">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Login</h1>
-        <form className="max-w-md mx-auto">
-          <input type="email" name="" placeholder="Your Email@com" />
-          <input type="password" name="" placeholder="Enter Password" />
+        <form className="max-w-md mx-auto" onSubmit={onLoginUser}>
+          <input
+            type="email"
+            name=""
+            placeholder="Your Email@com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            name=""
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button type="submit" className="primary">
             Login
           </button>
