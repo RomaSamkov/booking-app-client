f1db3405aed16ac37471b18ad7b6e98f3df6a469
RomaSamkov <Romansamkov.rs@gmail.com>
2024-01-29 17:15:13 +0200

    add UserContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from "./pages/LoginPage";
 import Layout from "./components/Layout";
 import RegisterPage from "./pages/RegisterPage";
 import axios from "axios";
+import { UserContextProvider } from "./components/UserContext";
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
@@ -13,13 +14,15 @@ axios.defaults.withCredentials = true;
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   );
 }
 
