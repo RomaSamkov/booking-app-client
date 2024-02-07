75cc08a73d2aae435cf489d7e6be10b7e92117ac
RomaSamkov <Romansamkov.rs@gmail.com>
2024-02-07 13:27:17 +0200

    add image

--- src/pages/PlacesPage.jsx
+++ src/pages/PlacesPage.jsx
@@ -1,13 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function PlacesPage() {
+  const { action } = useParams();
+  console.log(action);
   return (
     <div>
       <div className="text-center">
         <Link
-          className="bg-primary text-white rounded-full py-2 px-6"
+          className="inline-flex gap-1 bg-primary text-white rounded-full py-2 px-6"
           to={"/account/places/new"}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+              clipRule="evenodd"
+            />
+          </svg>
           Add new place
         </Link>
       </div>
