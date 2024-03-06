98e911ade57d50896223794ee517ff18589c8d39
RomaSamkov <Romansamkov.rs@gmail.com>
2024-03-06 13:34:44 +0200

    Update IndexPage.jsx

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function IndexPage() {
   const [places, setPlaces] = useState([]);
@@ -14,20 +15,19 @@ export default function IndexPage() {
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8 gap-x-6 gap-y-8">
       {places.length > 0 &&
         places.map((place) => (
-          <div key={place.title}>
+          <Link to={"/place/" + place._id} key={place._id}>
             <div>
-              <div>
-                {place.photos?.[0] && (
-                  <img
-                    className="border mb-2 rounded-2xl object-cover aspect-square"
-                    src={"http://localhost:3000/uploads/" + place.photos?.[0]}
-                  ></img>
-                )}
-              </div>
-              <h2 className="text-sm truncate">{place.title}</h2>
-              <h3 className="font-bold">{place.address}</h3>
+              {place.photos?.[0] && (
+                <img
+                  className="border mb-2 rounded-2xl object-cover aspect-square"
+                  src={"http://localhost:3000/uploads/" + place.photos?.[0]}
+                ></img>
+              )}
             </div>
-          </div>
+            <h2 className="font-bold">{place.address}</h2>
+            <h3 className="text-sm truncate text-gray-500">{place.title}</h3>
+            <div className="mt-1 font-bold">${place.price}</div>
+          </Link>
         ))}
     </div>
   );
