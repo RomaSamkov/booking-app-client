dff5488b58c0164f4e2b9cea9528f0de7d75509a
RomaSamkov <Romansamkov.rs@gmail.com>
2024-03-14 17:09:07 +0200

    Update BookingsPage.jsx

--- src/pages/BookingsPage.jsx
+++ src/pages/BookingsPage.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import AccountNav from "../components/AccountNav";
 import axios from "axios";
 import PlaceImg from "../components/PlaceImg";
-import { differenceInCalendarDays, format } from "date-fns";
 import { Link } from "react-router-dom";
+import BookingsInfo from "../components/BookingsInfo";
 
 export default function BookingsPage() {
   const [bookings, setBookings] = useState([]);
@@ -27,22 +27,7 @@ export default function BookingsPage() {
               <div className="w-72">
                 <PlaceImg place={booking.place} className="object-cover" />
               </div>
-              <div className="py-3 pr-4 grow">
-                <h2 className="text-xl">{booking.place.title}</h2>
-                <div className="border-t border-gray-600 mt-2 py-2">
-                  chek-in: {format(new Date(booking.checkIn), "yyyy-MM-dd")}{" "}
-                  check-out: {format(new Date(booking.checkOut), "yyyy-MM-dd")}
-                </div>
-                <div>
-                  Number of nights:{" "}
-                  {differenceInCalendarDays(
-                    new Date(booking.checkOut),
-                    new Date(booking.checkIn)
-                  )}{" "}
-                  <br />
-                  Price: ${booking.price}
-                </div>
-              </div>
+              <BookingsInfo booking={booking} />
             </Link>
           ))}
       </div>
